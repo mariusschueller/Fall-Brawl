bc1fd54c0c79263ae029b3712f483733f70acfb6
snagasuri <36570119+snagasuri@users.noreply.github.com>
2024-09-22 19:14:08 -0500

    added more scenes

---
+++ Assets/Scripts/CubeGenerator.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeGridGenerator : MonoBehaviour
+{
+    public GameObject cubePrefab; // assign your cube prefab in the inspector
+    public int rows = 4;
+    public int cols = 8;
+    public float spacing = 1.0f; // adjust if your cubes are bigger or smaller than 1 unit
+
+    void Start()
+    {
+        GenerateGrid();
+    }
+
+    void GenerateGrid()
+    {
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                Vector3 position = new Vector3(i * spacing, 0, j * spacing);
+                Instantiate(cubePrefab, position, Quaternion.identity);
+            }
+        }
+    }
+}
